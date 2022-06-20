1b3553bc58a02e4fd59d37c88ec8fe24d76702b9
Ho Ho Yin <hoyin@Hode-MacBook-Pro.local>
2022-06-21 04:24:17 +0900

    fixed dropdown menu bugs

--- components/DropdownMenuRow.tsx
+++ components/DropdownMenuRow.tsx
@@ -3,25 +3,23 @@ import Image from 'next/image'
 
 function DropdownMenuRow({coin}) {
   const [image, setImage] = useState('https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880');
-  
 
   useEffect(() => {
-    if (coin) {
+    if (coin)
       setImage(coin?.image);
-    }
   }, [coin])
   
   return (
-    <div className="flex space-x-2 m-2">
+    <div 
+    onClick={() => console.log(coin?.name)}
+    className="flex space-x-2 p-2 rounded-xl hover:bg-slate-700">
       <Image className='bg-white rounded-full' src={image} width={30} height={30} /> 
-				<div className='flex space-x-2 items-baseline'>
-					<p className='text-xl text-white font-bold'>{coin?.symbol.toUpperCase()}</p>
-					<p className='text-gray-400'>{coin?.name}</p>
-				</div>
+      <div className='flex space-x-2 items-baseline'>
+        <p className='text-xl text-white font-bold'>{coin?.symbol.toUpperCase()}</p>
+        <p className='text-gray-400'>{coin?.name}</p>
+      </div>
     </div>
   )
 }
-
-
 
 export default DropdownMenuRow
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,7 @@ import Head from 'next/head';
 import Feed from '../components/Feed';
 import Menu from '../components/Menu';
 
-const Home: NextPage = (props: any) => {
-  console.log(props?.data);
+const Home: NextPage = () => {
   return (
     <div className="mx-auto bg-slate-100">
       <Head>
@@ -19,14 +18,6 @@ const Home: NextPage = (props: any) => {
       </main>
     </div>
   )
-};
-
-export const getServerSideProps = async () => {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
-  const coins = await res.json();
-  return {
-    props: { data: coins },
-  };
 };
 
 export default Home
